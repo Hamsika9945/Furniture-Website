631b2fd6210e8a9b43147e51e716f92c7fc62393
Hamsika9945 <karnakotahamsika@gmail.com>
2025-05-11 17:06:09 +0530

    Delete 1.js

--- 1.js
+++
@@ -1,5 +0,0 @@
-const emailInput = document.getElementById('email');
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-if (!emailInput.value.match(emailRegex)) {
-  alert('Invalid email address.');
-}
